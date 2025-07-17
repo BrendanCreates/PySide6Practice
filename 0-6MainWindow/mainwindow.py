e656d2c52ccfa539062f86d4f11342fc2e22e195
BrendanCreates <bnevilsstem2023@gmail.com>
2025-07-16 20:22:31 -0400

    Did some more

--- 0-6MainWindow/mainwindow.py
+++ 0-6MainWindow/mainwindow.py
@@ -1,4 +1,7 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+# We need to import QSize from QtCore so we can set the size of the icons
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QToolBar
 
 # we are going to inheret from QMainWindow and make this class that basically just sets up a main window constructred from the main window base object
 class MainWindow(QMainWindow):
@@ -11,5 +14,47 @@ class MainWindow(QMainWindow):
 
         # menubar and menus
         menu_bar = self.menuBar()
-        # the & means the F key becomes the access key (Alt + F will open the menu)
+        # the & ("&File")means the F key becomes the access key (Alt + F will open the menu)
         file_menu = menu_bar.addMenu("&File")
+        quit_action = file_menu.addAction("Quit")
+        quit_action.triggered.connect(self.quit_app)
+
+        edit_menu = menu_bar.addMenu("Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+
+        menu_bar.addMenu("Window")
+        menu_bar.addMenu("Setting")
+        menu_bar.addMenu("Help")
+
+
+
+        toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+
+        toolbar.addAction(quit_action) # Doesn't just have to be a name as we can pass in an already existing action as seen here
+
+        # Qt / PySide has us pass in the parent object as the second parameter in many classes
+        # Qt uses a parent child hierarchy to manage object lifetimes so everything can be properly managed when something is deleted
+        # which also prevents memory leakage
+        # so action1 is considered a child of MainWindow
+        action1 = QAction("Some Action", self)
+        # this line was not included in the course but the following line
+        # another (redundant) way to do this is self.setStatusBar(self.statusBar()) where statusBar is created if it doesnt exist from self.statusBar() and is returned, to the setStatusBar method and that sets the returned status bar
+        status_bar = self.statusBar()
+        # sets message in status bar which is a place at the bottom of the screen where tips and info show up
+        action1.setStatusTip("Status message for some action")
+        action1.triggered.connect(self.toolbar_button_click)
+        toolbar.addAction(action1)
+
+
+
+    def quit_app(self):
+        self.app.quit()
+    
+    def toolbar_button_click(self):
+        print("action1 triggered")
